3de7180e247c85e5444aeee47924732be0350edb
Minokkk <kki38900758@gmail.com>
2020-03-28 16:54:15 +0900

    Test

---
+++ Assets/Script/Tile/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _tileSpriteRenderer;
+    public SpriteRenderer _TileSpriteRenderer
+    {
+        get { return _tileSpriteRenderer; }
+    }
+
+    [SerializeField] private Vector2 _tilePosIndex=new Vector2();
+    public Vector2 _TilePosIndex
+    {
+        get { return _tilePosIndex; }
+    }
+
+    public void Initialize(Vector2 _tilePosIdx, float _distance)
+    {
+        _tilePosIndex = _tilePosIdx;
+
+        transform.localPosition = _tilePosIdx * _distance;
+    }
+}
