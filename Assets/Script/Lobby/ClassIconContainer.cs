0d6c1c20c2c2d9a3a48b1700f53c221a0748644b
Minokkk <kki38900758@gmail.com>
2020-07-18 15:03:10 +0900

    Lobby Edit

---
+++ Assets/Script/Lobby/ClassIconContainer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassIconContainer : MonoBehaviour
+{
+    public enum ClassType
+    {
+        Null,
+        Warrior,
+        Archer,
+        Wizard,
+        Assassin,
+        Supporter
+    }
+    public static ClassIconContainer Instance;
+
+    [SerializeField] private Sprite _warriorSprite;
+    [SerializeField] private Sprite _archerSprite;
+    [SerializeField] private Sprite _wizardSprite;
+    [SerializeField] private Sprite _assassinSprite;
+    [SerializeField] private Sprite _supporterSprite;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    public Sprite GetClassIcon(EnumInfo.ClassType _type)
+    {
+        switch(_type)
+        {
+            case EnumInfo.ClassType.Warrior:
+                return _warriorSprite;
+            case EnumInfo.ClassType.Archer:
+                return _archerSprite;
+            case EnumInfo.ClassType.Wizard:
+                return _wizardSprite;
+            case EnumInfo.ClassType.Assassin:
+                return _assassinSprite;
+            case EnumInfo.ClassType.Supporter:
+                return _supporterSprite;
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Script/Lobby/LobbyUnitInfoUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lobby
+{
+    public class LobbyUnitInfoUI : MonoBehaviour
+    {
+        [Header("Info")]
+        [SerializeField] private Text _unitNameText;
+        [SerializeField] private Image _unitClassImage;
+        [SerializeField] private Text _maxReinforceText;
+
+        [SerializeField] private string _unitName;
+        private UnitData _unitData;
+
+        public void Start()
+        {
+            _unitData = DataManager.Instance.GetUnitDataWithUnitName(_unitName);
+
+            InitUI();
+        }
+
+        public void InitUI()
+        {
+            _unitNameText.text = _unitData.UnitName;
+            _unitClassImage.sprite = ClassIconContainer.Instance.GetClassIcon((EnumInfo.ClassType)System.Enum.Parse(typeof(EnumInfo.ClassType),_unitData.Class));
+
+            _maxReinforceText.text = _unitData.MaxReinforce.ToString();
+        }
+    }
+}
