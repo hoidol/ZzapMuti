77a317c9ee1c425df924acd4a9ff4237060cdfa8
dropnut <qkrghdud0@gmail.com>
2020-07-18 13:29:03 +0900

    로비에서 덱 가져오기 전 백업

---
+++ Assets/Script/Player/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+
+    public void InitPlayerMgr()
+    {
+        //내 덱과 상대방 덱 가져오기
+
+
+    }
+
+}
