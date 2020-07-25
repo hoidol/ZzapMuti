b336275f8f1d2c12f7dbf91c279cf0211cb297f4
daniel <qkrghdud0@gmail.com>
2020-07-25 23:50:54 +0900

    AI 작ㄱ업중

--- Assets/Script/AI/AIManager.cs
+++ Assets/Script/AI/AIManager.cs
@@ -8,54 +8,54 @@ public class AIManager : MonoBehaviour
 
     UserData _opponentData;
     //List<UnitData> _opponentUnitDataList = new List<UnitData>();
-    List<string> _opponentUnitDataDeckList = new List<string>();
+    public List<UnitData> _opponentUnitDataDeckList = new List<UnitData>();
+
+    [SerializeField] AIPlayType[] _aiPlayTypes;
+
+    [SerializeField] AIPlayType _curAIPlayType;
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
     }
 
-    public void SetOpponentData(UserData _oData)
+    public void InitAIMgr(UserData _oData)
     {
-       for(int i =0;i< _oData.UnitInven.Length; i++)
+        _aiPlayTypes = GetComponentsInChildren<AIPlayType>();
+        for(int i =0;i< _aiPlayTypes.Length; i++)
+            _aiPlayTypes[i].InitAIPlayType();
+
+       for (int i =0;i< _oData.UnitInven.Length; i++)
         {
             UnitData _uData = DataManager.Instance.GetUnitDataWithUnitIdx(_oData.UnitInven[i]);
             //_opponentUnitDataList.Add(_uData);
             for(int j =0;j< _uData.MaxReinforce; j++)
-                _opponentUnitDataDeckList.Add(_uData.UnitIdx);
+                _opponentUnitDataDeckList.Add(_uData);
         }
+        _curAIPlayType = _aiPlayTypes[Random.Range(0, _aiPlayTypes.Length)];
     }
 
 
-    public void StartTurn(EnumInfo.TeamType _tT)
+    public void StartTurn(int _r)
     {
         // 3개를 랜덤으로 뽑고
         // 그 중 하나를 선택
-        int random1;
-        int random2;
+       
 
-        string tmp;
-        for (int i = 0; i < _opponentUnitDataDeckList.Count; ++i)
-        {
-            random1 = Random.Range(0, _opponentUnitDataDeckList.Count);
-            random2 = Random.Range(0, _opponentUnitDataDeckList.Count);
-
-            tmp = _opponentUnitDataDeckList[random1];
-            _opponentUnitDataDeckList[random1] = _opponentUnitDataDeckList[random2];
-            _opponentUnitDataDeckList[random2] = tmp;
-        }
-
-        
+        _curAIPlayType.StartTurn(_r);
+        //UnitManager.Instance._curPlayerUnitsOnTile
     }
 
-    //고려해야 할 부분 : 1.  어떤 기준 유닛을 뽑을 지 2. 유닛을 어떤 기준으로 어디에 배치할지   
-    // 1. 어떤 기준 유닛을 뽑을지에 대해서
-    // 1)
 
 
     public void FinishTurn()
     {
 
+    }
+
+    public void PickUnitCard(UnitData _uData)
+    {
+        _opponentUnitDataDeckList.Remove(_uData);
     }
 
 }
