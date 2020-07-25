a484412ffdcd9e78c9679a7610ffb24454d3e923
Minokkk <kki38900758@gmail.com>
2020-07-25 22:11:39 +0900

    파이어 베이스 삽입

---
+++ Assets/Script/DB/FireBaseTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class FireBaseTest : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+  var dependencyStatus = task.Result;
+  if (dependencyStatus == Firebase.DependencyStatus.Available) {
+    // Create and hold a reference to your FirebaseApp,
+    // where app is a Firebase.FirebaseApp property of your application class.
+    //   app = Firebase.FirebaseApp.DefaultInstance;
+
+    // Set a flag here to indicate whether Firebase is ready to use by your app.
+  } else {
+    UnityEngine.Debug.LogError(System.String.Format(
+      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+    // Firebase Unity SDK is not safe to use here.
+  }
+});
+    }
+
+    void Start()
+    {
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://zzapmuti.firebaseio.com/");
+        Debug.Log("가져옴");
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        GetData();
+    }
+
+    public void GetData()
+    {
+        FirebaseDatabase.DefaultInstance
+       .GetReference("zzapmuti")
+       .GetValueAsync().ContinueWith(task =>
+       {
+           if (task.IsFaulted)
+           {
+               Debug.Log(task.AsyncState);
+               // Handle the error...
+           }
+           else if (task.IsCompleted)
+           {
+               DataSnapshot snapshot = task.Result;
+
+               //for(int i = 0; i < snapshot.ChildrenCount; i++)
+               //{
+               //    IEnumerable _i = snapshot[i];
+               //}
+               Debug.Log(snapshot.GetRawJsonValue());
+               // Do something with snapshot...
+           }
+       });
+    }
+}
