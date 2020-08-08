d8bd097ffee170ea349811daef28c935996428d8
Minokkk <kki38900758@gmail.com>
2020-08-08 13:58:52 +0900

    파이어베이스 테스팅

---
+++ Assets/Script/DB/GameAuthControl.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Unity.Editor;
+
+public class GameAuthControl : MonoBehaviour
+{
+    private GameAuthControl instance;
+    public GameAuthControl Instance
+    {
+        get { return instance; }
+    }
+
+    private FirebaseAuth auth;
+    public FirebaseAuth Auth
+    {
+        get { return auth; }
+    }
+
+    private FirebaseUser user;
+
+    public void Awake()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        InitializeFirebase();
+    }
+
+    public void OnDestroy()
+    {
+        auth.SignOut();
+    }
+
+    // Handle initialization of the necessary firebase modules:
+    void InitializeFirebase()
+    {
+        Debug.Log("Setting up Firebase Auth");
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    }
+
+    public void CreateUser(string _email,string _password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(_email, _password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+        });
+    }
+
+    public void ChangeDisplayName(string _name)
+    {
+        UserProfile userProfile = new UserProfile();
+        userProfile.DisplayName = _name;
+        userProfile.PhotoUrl = user.PhotoUrl;
+
+        user.UpdateUserProfileAsync(userProfile);
+    }
+    
+
+    public void Login(string _email, string _password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(_email, _password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
---
+++ Assets/Script/DB/UserDataSave.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Threading.Tasks;
+
+public class UserDataSave : MonoBehaviour
+{
+    private static UserDataSave instance;
+    public static UserDataSave Instance
+    {
+        get { return instance; }
+    }
+
+    private DatabaseReference _databaseReference;
+
+    public void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        Init();   
+    }
+
+    public void Init()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://zzapmuti.firebaseio.com/");
+
+        _databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    private void SaveUserData(string _nickname,string[] _unitInven)
+    {
+        UserData user = new UserData() { nickName= _nickname, UnitInven=_unitInven};
+        string json = JsonUtility.ToJson(user);
+
+        Task saveTask = 
+            _databaseReference.Child("PlayerData").
+            Child("dHOSUJyXDWnHVLCqCgBr").SetRawJsonValueAsync(json);
+
+        if (!saveTask.IsFaulted)
+        {
+            Debug.Log("세이브 성공");
+        }
+        else
+        {
+            Debug.Log("세이브 실패");
+        }
+    }
+
+    public void GetUserData()
+    {
+        _databaseReference
+       .Child("PlayerData").Child("dHOSUJyXDWnHVLCqCgBr")
+       .GetValueAsync().ContinueWith(task =>
+       {
+           if (task.IsFaulted)
+           {
+               Debug.Log("로드 실패");
+               Debug.Log(task.AsyncState);
+               // Handle the error...
+           }
+           else if (task.IsCompleted)
+           {
+               Debug.Log("로드 성공");
+               DataSnapshot snapshot = task.Result;
+
+               //Debug.Log(snapshot.GetRawJsonValue());
+               JsonToUserData(snapshot.GetRawJsonValue());
+               // Do something with snapshot...
+           }
+       });
+    }
+
+    private void JsonToUserData(string _jsonData)
+    {
+        UserData data;
+
+        data = JsonUtility.FromJson<UserData>(_jsonData);
+
+        Debug.Log(data.nickName);
+        Debug.Log(data.UnitInven);
+    }
+}
